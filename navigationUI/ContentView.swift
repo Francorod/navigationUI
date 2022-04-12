f98bb914f50da03e0e142821407f95aabbc0bda5
Franco Rod <francorodios@gmail.com>
2022-04-12 18:00:04 -0400

    Initial Commit

---
+++ navigationUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  navigationUI
+//
+//  Created by Franco Rodrigues on 4/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ navigationUI/navigationUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  navigationUIApp.swift
+//  navigationUI
+//
+//  Created by Franco Rodrigues on 4/12/22.
+//
+
+import SwiftUI
+
+@main
+struct navigationUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
