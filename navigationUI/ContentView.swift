b9d6b9a56188e935f22e1389253d97d00b04b222
Franco Rod <francorodios@gmail.com>
2022-04-12 18:07:31 -0400

    First part = added button.

--- navigationUI/ContentView.swift
+++ navigationUI/ContentView.swift
@@ -8,9 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isShowingDetailView = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack(spacing: 30) {
+                NavigationLink(destination: Text("Second View"), isActive: $isShowingDetailView) { EmptyView()}
+                
+                Button("Tap to show detail") {
+                    self.isShowingDetailView = true
+                }
+            }
+            .navigationTitle("Navigation")
+        }
     }
 }
 
